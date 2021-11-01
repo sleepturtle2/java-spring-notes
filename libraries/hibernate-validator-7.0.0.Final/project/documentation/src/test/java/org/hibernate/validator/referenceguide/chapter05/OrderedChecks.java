11fd075c72b537d7d6b598d0a82c72e9dddf3e96
sleepturtle2 <sayantan.mukh98@gmail.com>
2021-11-01 20:05:59 +0530

    removed hibernate lib jar files

--- libraries/hibernate-validator-7.0.0.Final/project/documentation/src/test/java/org/hibernate/validator/referenceguide/chapter05/OrderedChecks.java
+++
@@ -1,8 +0,0 @@
-package org.hibernate.validator.referenceguide.chapter05;
-
-import jakarta.validation.GroupSequence;
-import jakarta.validation.groups.Default;
-
-@GroupSequence({ Default.class, CarChecks.class, DriverChecks.class })
-public interface OrderedChecks {
-}
--- libraries/hibernate-validator-7.0.0.Final/project/documentation/src/test/java/org/hibernate/validator/referenceguide/chapter05/groupinheritance/RaceCarChecks.java
+++
@@ -1,6 +0,0 @@
-package org.hibernate.validator.referenceguide.chapter05.groupinheritance;
-
-import jakarta.validation.groups.Default;
-
-public interface RaceCarChecks extends Default {
-}
